9a7ac6b22b1b871d2a4470e88129476f20dd2355
Michael Stramel <stramel@users.noreply.github.com>
2017-05-03 17:31:44 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -155,16 +155,16 @@ function build() {
         // waitFor the buildStream to complete
         return waitFor(buildStream);
       })
-      .then(() => {
-        // Okay, now let's generate the Service Worker
-        console.log('Generating the Service Worker...');
-        return polymerBuild.addServiceWorker({
-          project: polymerProject,
-          buildRoot: buildDirectory,
-          bundled: true,
-          swPrecacheConfig: swPrecacheConfig
-        });
-      })
+//       .then(() => {
+//         // Okay, now let's generate the Service Worker
+//         console.log('Generating the Service Worker...');
+//         return polymerBuild.addServiceWorker({
+//           project: polymerProject,
+//           buildRoot: buildDirectory,
+//           bundled: true,
+//           swPrecacheConfig: swPrecacheConfig
+//         });
+//       })
       .then(() => {
         // You did it!
         console.log('Build complete!');
